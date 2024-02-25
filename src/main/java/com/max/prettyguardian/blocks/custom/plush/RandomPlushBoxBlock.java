98fe4909f60915998ae8f21f20d3bf17c6294960
Maxime Tytgat <maxime2103i@gmail.com>
2024-02-25 01:36:30 +0100

    Feat(block): End RandomPlushBoxBlock :truck::truck:

--- src/main/java/com/max/prettyguardian/blocks/custom/plush/RandomPlushBoxBlock.java
+++ src/main/java/com/max/prettyguardian/blocks/custom/plush/RandomPlushBoxBlock.java
@@ -2,16 +2,48 @@ package com.max.prettyguardian.blocks.custom.plush;
 
 import com.max.prettyguardian.blocks.PrettyGuardianBlock;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.Containers;
 import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Random;
 
 public class RandomPlushBoxBlock extends Block {
+
+    public static final EnumProperty<Direction> FACING = DirectionalBlock.FACING;
+    private static final VoxelShape SHAPE_NORTH = Shapes.or(
+            Block.box(4, 4, 4, 12, 8, 12),
+            Block.box(4.25, 0, 4.25, 11.75, 4, 11.75),
+            Block.box(7, 3, 11.700000000000001, 9, 5.0009999999999994, 12.500000000000002)
+    );
+    private static final VoxelShape SHAPE_SOUTH = Shapes.or(
+            Block.box(4, 4, 4, 12, 8, 12),
+            Block.box(4.25, 0, 4.25, 11.75, 4, 11.75),
+            Block.box(7, 3, 3.5, 9, 5.0009999999999994, 4.300000000000001)
+    );
+    private static final VoxelShape SHAPE_WEST = Shapes.or(
+            Block.box(4, 4, 4, 12, 8, 12),
+            Block.box(4.25, 0, 4.25, 11.75, 4, 11.75),
+            Block.box(11.700000000000001, 3, 7, 12.500000000000002, 5.0009999999999994, 9)
+    );
+    private static final VoxelShape SHAPE_EAST = Shapes.or(
+            Block.box(4, 4, 4, 12, 8, 12),
+            Block.box(4.25, 0, 4.25, 11.75, 4, 11.75),
+            Block.box(3.5, 3, 7, 4.300000000000001, 5.0009999999999994, 9)
+    );
     public RandomPlushBoxBlock(Properties properties) {
         super(properties);
     }
@@ -37,5 +69,27 @@ public class RandomPlushBoxBlock extends Block {
         Containers.dropContents(level, blockPos, inventory);
 
         super.onRemove(blockState, level, blockPos, blockState1, p_60519_);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState p_60555_, BlockGetter p_60556_, BlockPos p_60557_, CollisionContext p_60558_) {
+        return switch (p_60555_.getValue(FACING)) {
+            case NORTH -> SHAPE_SOUTH;
+            case SOUTH -> SHAPE_NORTH;
+            case WEST -> SHAPE_EAST;
+            case EAST -> SHAPE_WEST;
+            default -> SHAPE_NORTH;
+        };
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
     }
 }
