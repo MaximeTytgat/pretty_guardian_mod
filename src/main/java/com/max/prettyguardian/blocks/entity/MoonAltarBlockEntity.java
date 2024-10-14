2fc06c51049b877e179d4c7b181b55377dccd60a
Maxime Tytgat <maxime2103i@gmail.com>
2024-10-14 13:39:52 +0200

    Feat 1.20.1

--- src/main/java/com/max/prettyguardian/blocks/entity/MoonAltarBlockEntity.java
+++ src/main/java/com/max/prettyguardian/blocks/entity/MoonAltarBlockEntity.java
@@ -19,7 +19,6 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.RandomizableContainerBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
